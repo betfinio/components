7c687e8932b610c2e3bd7b568846eaee9857babc
Monster <bf.monster@proton.me>
2025-01-20 09:51:14 +0100

    fix: fixed build

--- rslib.config.ts
+++ rslib.config.ts
@@ -23,11 +23,6 @@ export default defineConfig({
 		target: 'web',
 		cleanDistPath: true,
 		minify: true,
-		externals: {
-			'framer-motion': 'framer-motion',
-			clsx: 'clsx',
-			'tailwind-merge': 'tailwind-merge',
-		},
 	},
 	tools: {
 		rspack: {

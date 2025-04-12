2e6bbc75c482439f687696ddb77f667b3d1476ae
bf-san <bf-san@proton.me>
2025-04-12 11:54:49 +0200

    feat: updated toast styles

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,3 +1,4 @@
+import { Loader, Loader2 } from 'lucide-react';
 import { Toaster as Sonner, toast } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
@@ -7,12 +8,22 @@ const SonnerToaster = ({ ...props }: ToasterProps) => {
 		<Sonner
 			theme={'dark'}
 			className="toaster group"
+			icons={{
+				loading: <Loader className="w-4 h-4 animate-spin " />,
+			}}
 			toastOptions={{
+				unstyled: true,
+
 				classNames: {
-					toast: 'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+					toast:
+						'data-[sonner-toast]:w-[var(--width)] group toast  text-foreground bg-background-lighter border shadow-lg flex items-center rounded-lg px-4 py-2 gap-2',
 					description: 'group-[.toast]:text-muted-foreground',
-					actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+
 					cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+					success: 'border-success [&_svg]:text-success',
+					error: 'border-destructive [&_svg]:text-destructive',
+					loader: ' [&_svg]:text-foreground !static !transform-none data-[visible="false"]:hidden',
+					actionButton: 'ml-auto',
 				},
 			}}
 			{...props}

c42c6b9f2eb003947584ad20356b3be219064980
bf-monster <bf.monster@proton.me>
2025-05-16 11:44:09 +0200

    fix: remove close button from SheetContent component

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -48,10 +48,6 @@ const SheetContent = ({ side = 'right', className, children, ...props }: SheetCo
 		<SheetOverlay />
 		<SheetPrimitive.Content data-slot="sheet-content" className={cn(sheetVariants({ side }), className)} {...props}>
 			{children}
-			<SheetPrimitive.Close className="absolute right-6 top-6 rounded-sm opacity-70 ring-offset-primary transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-				<X className="h-4 w-4" />
-				<span className="sr-only">Close</span>
-			</SheetPrimitive.Close>
 		</SheetPrimitive.Content>
 	</SheetPortal>
 );

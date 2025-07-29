b1b25c35d7ed9832efde6b8fc2662cf41a64cd9a
bf-monster <bf.monster@proton.me>
2025-07-29 15:51:36 +0200

    fix: added react

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
-import type * as React from 'react';
+import type React from 'react';
 import type { DayButton } from 'react-day-picker';
 import { DayPicker, getDefaultClassNames } from 'react-day-picker';
 import { Button, buttonVariants } from '@/components/ui/button';
